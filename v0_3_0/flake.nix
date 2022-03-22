00d7de4977e6775c560a9a0a21d323966f56259b
hugosenari <hugosenari@gmail.com>
2022-03-22 01:08:07 -0300

    chore: re-index ../nimpkgs/c/colorcol

--- v0_3_0/flake.nix
+++ v0_3_0/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-colorcol-v0_3_0.flake = false;
+  inputs.src-colorcol-v0_3_0.ref   = "refs/tags/v0.3.0";
   inputs.src-colorcol-v0_3_0.owner = "SolitudeSF";
-  inputs.src-colorcol-v0_3_0.ref   = "v0_3_0";
   inputs.src-colorcol-v0_3_0.repo  = "colorcol";
   inputs.src-colorcol-v0_3_0.type  = "github";
   
